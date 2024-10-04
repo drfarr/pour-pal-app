7f5fbaa70dece5bda9594287226c6107db79525e
Dan Farr <dan.farr@gmail.com>
2024-10-04 17:30:48 +0100

    fix: Adding alias paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,6 +11,11 @@ export default defineConfig(({ mode }) => {
       "process.env": env,
     },
     plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     test: {
       globals: true,
       environment: "jsdom",
